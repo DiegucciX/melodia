a02935a1cfbcb0c0585c9eb68f2a51474f1b8e9c
DiegucciX <98874031+DiegucciX@users.noreply.github.com>
2022-02-02 09:18:18 +0100

    ESPAÑITA!!!

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    music.setTempo(35)
     music.playTone(349, music.beat(BeatFraction.Quarter))
     music.playTone(262, music.beat(BeatFraction.Quarter))
     music.playTone(440, music.beat(BeatFraction.Quarter))
@@ -12,7 +13,16 @@ input.onButtonPressed(Button.A, function () {
     music.playTone(330, music.beat(BeatFraction.Eighth))
     music.playTone(294, music.beat(BeatFraction.Eighth))
     music.playTone(262, music.beat(BeatFraction.Eighth))
-    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(466, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
 })
 basic.showLeds(`
     . # # # #
